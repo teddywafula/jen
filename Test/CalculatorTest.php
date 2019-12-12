72c90937448990b26dc4be09c11fd37406b597cc
teddywafula <teddywafula@gmail.com>
2019-12-12 12:49:30 +0300

    First Commit

---
+++ Test/CalculatorTest.php
@@ -0,0 +1,21 @@
+<?php
+require_once("src/Calculator.php");
+use PHPUnit\Framework\TestCase; 
+class CalculatorTest extends TestCase
+{
+ 
+    public function testAdd()
+    {
+        $this->calculator = new Calculator();
+        $result = $this->calculator->add(1, 2);
+        $this->assertEquals(3, $result);
+        $this->assertEquals(1,1);
+    }
+    public function testAdds(){
+        $this->calculator = new Calculator();
+        $result = $this->calculator->adds(2);
+
+    	$this->assertEquals($result,2);
+    }
+ 
+}
---
+++ src/Calculator.php
@@ -0,0 +1,16 @@
+<?php
+class Calculator
+{
+ 
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+     public function adds($a)
+    {
+        return $a;
+
+    }
+ 
+}
